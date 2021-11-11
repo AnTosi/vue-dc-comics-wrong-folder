ca1214d165a3d446b9c94723485393bc14143db3
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-11 13:26:42 +0100

    abbozzato header e section 1

--- dc_comics/src/App.vue
+++ dc_comics/src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="app">
-    <header id="site_header d-flex container">
+    <header id="site_header" class="d-flex container">
       <div class="logo col-6">
+        <img src="./assets/dc-logo.png" alt="">
       </div>
-      
-      <div class="nav col-6">
+
+      <div class="nav col-6 d-flex align-items-center">
+        <ul class="d-flex">
+          <li>CHARACTERS</li>
+          <li>COMICS</li>
+          <li>MOVIES</li>
+          <li>TV</li>
+          <li>GAMES</li>
+          <li>COLLECTIBLES</li>
+          <li>VIDEOS</li>
+          <li>FANS</li>
+          <li>NEWS</li>
+          <li>SHOP</li>
+        </ul>
       </div>  
     </header>
-    <main class="container">
+    <main>
       <section id="section_one" class="bg_black">
-
+        <div class="text_white py-2 font-size-2 container bg_black">
+        --> Content goes here chevronleft__
+        </div>
       </section>
 
-      <section id="section_two" class="d-flex justify_content_around align_content_center">
+      <section id="section_two" class="d-flex justify_content_around align_items_center">
         fascia blu
       </section>
     </main>
@@ -48,4 +63,7 @@ export default {
 //   color: #2c3e50;
 //   margin-top: 60px;
 // }
+
+@import './assets/variables.scss';
+@import './assets/common.scss';
 </style>
