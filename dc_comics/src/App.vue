993cb45538ab73f63be5d07fc2a4119ebc0efa99
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-11 15:13:36 +0100

    pusho prima di provare a copiare tutto fuori dalla sottocartella

--- dc_comics/src/App.vue
+++ dc_comics/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <header id="site_header" class="d-flex container">
-      <div class="logo col-6">
+      <div class="logo col-40">
         <img src="./assets/dc-logo.png" alt="">
       </div>
 
-      <div class="nav col-6 d-flex align-items-center">
+      <div class="nav col-60 d-flex align-items-center">
         <ul class="d-flex">
           <li>CHARACTERS</li>
           <li>COMICS</li>
@@ -22,13 +22,38 @@
     </header>
     <main>
       <section id="section_one" class="bg_black">
-        <div class="text_white py-2 font-size-2 container bg_black">
-          --> Content goes here --
+        <div class="text_white py-4 font-size-2 container bg_black">
+          --> Content goes here &#60;--
         </div>
       </section>
 
-      <section id="section_two" class="d-flex justify_content_around align_items_center">
-        fascia blu
+      <section id="section_two" class="d-flex justify-content-around bg_blue py-5">
+          <div class="container d-flex justify-content-around text_white">
+            <div class="col-20 d-flex align-items-center">
+              <img src=".\assets\buy-comics-digital-comics.png" alt="">
+              <div class="d-flex vertical-middle line-height-50 px-2">DIGITAL COMICS</div>
+            </div>
+
+            <div class="col-20 d-flex align-items-center">
+              <img src=".\assets\buy-comics-merchandise.png" alt="">
+              <div class="d-flex vertical-middle line-height-50 px-2">DC MERCHANDISE</div>
+            </div>
+
+            <div class="col-20 d-flex align-items-center">
+              <img src=".\assets\buy-comics-subscriptions.png" alt="">
+              <div class="d-flex vertical-middle line-height-50 px-2">SUBSCRIPTION</div>
+            </div>
+
+            <div class="col-20 d-flex align-items-center">
+              <img src=".\assets\buy-comics-shop-locator.png" alt="">
+              <div class="d-flex vertical-middle line-height-50 px-2">COMIC SHOP LOCATOR</div>
+            </div>
+
+            <div class="col-20 d-flex align-items-center">
+              <img src=".\assets\buy-dc-power-visa.svg" alt="">
+              <div class="d-flex vertical-middle line-height-50 px-2">DC POWER VISA</div>
+            </div>
+          </div>
       </section>
     </main>
 
@@ -63,7 +88,9 @@ export default {
 //   color: #2c3e50;
 //   margin-top: 60px;
 // }
-
+#section_two img {
+  height: 50px;
+}
 @import './assets/variables.scss';
 @import './assets/common.scss';
 </style>
